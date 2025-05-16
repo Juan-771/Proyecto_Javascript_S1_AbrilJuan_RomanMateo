eeca3bea1c3d27e38b0ba3149ac365e48bf8892b
Mvteiio <romancamargo02@gmail.com>
2025-05-16 09:07:18 -0500

    Feat :rocket: Teams Page

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const user = document.getElementById("user").value
+    const password = document.getElementById("password").value
+
+    if (user === "Admin" && password === "admin123" ){
+        window.location.href = "../html/adminPages/adminPrincipalPage.html";
+    } 
+    else {
+        window.location.href = "../html/userPages/principalPage.html";
+    }
+})
